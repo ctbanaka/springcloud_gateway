5c04ea4bb79ead5faeddeff4a57a01346759855e
ctbanaka <103598029+ctbanaka@users.noreply.github.com>
2023-06-23 18:46:39 +0530

    changes in java version

--- src/main/java/com/oneview/gateway/GatewayApplication.java
+++ src/main/java/com/oneview/gateway/GatewayApplication.java
@@ -1,5 +1,4 @@
 package com.oneview.gateway;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
